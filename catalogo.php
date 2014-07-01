ed14a8dd71b80ab5c8e470fd1d84be353035c15b
ijmachado <noztalgik666@gmail.com>
2014-07-01 18:27:18 -0300

    animacion producto

--- catalogo.php
+++ catalogo.php
@@ -100,15 +100,15 @@
           </div>                
       </article>
       <article class="small-8 medium-8 large-9 column text-center">
-        <div class="small-12 medium-6 large-3 column text-center">
-            <a href="#">
-                <img src="r/img/articulo-1.jpg">
-                <h6 class="panel">
-                Articulo 1
-                </h6>
-            </a>
-        </div> 
-        <div class="small-12 medium-6 large-3 column text-center">
+        <div class="view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div>  
+        <!--<div class="small-12 medium-6 large-3 column text-center">
             <a href="#">
                 <img src="r/img/articulo-1.jpg">
                 <h6 class="panel">
@@ -131,7 +131,7 @@
                 Articulo 1
                 </h6>
             </a>
-        </div> 
+        </div> -->
       </article>
   </section>
   
