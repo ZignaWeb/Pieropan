f098ecc369c4f03b2dc021be5715b4805bc0d764
ijmachado <noztalgik666@gmail.com>
2014-07-01 19:24:09 -0300

    estilo

--- catalogo.php
+++ catalogo.php
@@ -16,91 +16,47 @@
   </header>
   <section class="small-12">
       <article id="side" class="small-4 medium-4 large-3 column">
+        <div class="small-12 large-6 text-center view view-first">  
+             <img src="r/img/rubro-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div>   
           <div class="wrap-thumbs small-12">
-          	<div class="small-12 large-6 column text-center">
-            	<a href="#" class="text-center">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div>            
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div>  			
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div>   
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div>  
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div> 
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div> 
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div> 
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div> 
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div> 
-            <div class="small-12 large-6 column text-center">
-            	<a href="#">
-                    <img src="r/img/rubro-1.jpg">
-                    <h6 class="panel">
-                    Rubro 1
-                    </h6>
-            	</a>
-            </div>     
-          </div>                
+          	<div class="small-12 large-6 text-center view view-first">  
+                 <img src="r/img/rubro-2.jpg" />  
+                 <div class="mask">  
+                 <h2>Producto</h2>  
+                 <p>Descripción</p>  
+                     <a href="#" class="info">Leer más</a>  
+                 </div>  
+            </div>    
+          </div>
+          <div class="wrap-thumbs small-12">
+          	<div class="small-12 large-6 text-center view view-first">  
+                 <img src="r/img/rubro-3.jpg" />  
+                 <div class="mask">  
+                 <h2>Producto</h2>  
+                 <p>Descripción</p>  
+                     <a href="#" class="info">Leer más</a>  
+                 </div>  
+            </div>    
+          </div>
+          <div class="wrap-thumbs small-12">
+          	<div class="small-12 large-6 text-center view view-first">  
+                 <img src="r/img/rubro-1.jpg" />  
+                 <div class="mask">  
+                 <h2>Producto</h2>  
+                 <p>Descripción</p>  
+                     <a href="#" class="info">Leer más</a>  
+                 </div>  
+            </div>    
+          </div>            
       </article>
       <article class="small-8 medium-8 large-9 column text-center">
-        <div class="view view-first">  
+        <div class="small-5 medium-5 large-3 view view-first">  
              <img src="r/img/articulo-1.jpg" />  
              <div class="mask">  
              <h2>Producto</h2>  
@@ -108,6 +64,30 @@
                  <a href="#" class="info">Leer más</a>  
              </div>  
         </div>  
+        <div class="small-5 medium-5 large-3 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
+        <div class="small-5 medium-5 large-3 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
+        <div class="small-5 medium-5 large-3 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
         <!--<div class="small-12 medium-6 large-3 column text-center">
             <a href="#">
                 <img src="r/img/articulo-1.jpg">
