c347001b484b98c64be982d82fe08a51d5ff4174
ijmachado <noztalgik666@gmail.com>
2014-07-07 16:56:19 -0300

    asd

--- catalogo.php
+++ catalogo.php
@@ -16,55 +16,37 @@
   </header>
   <section class="small-12">
       <article id="side" class="small-4 medium-4 large-3 column">
-        <div class="small-12 large-6 text-center view view-first">  
-             <img src="r/img/rubro-1.jpg" />  
-             <div class="mask">  
-             <h2>Producto</h2>  
-             <p>Descripción</p>  
-                 <a href="#" class="info">Leer más</a>  
-             </div>  
-        </div>   
-          <div class="wrap-thumbs small-12">
-          	<div class="small-12 large-6 text-center view view-first">  
-                 <img src="r/img/rubro-2.jpg" />  
-                 <div class="mask">  
-                 <h2>Producto</h2>  
-                 <p>Descripción</p>  
-                     <a href="#" class="info">Leer más</a>  
-                 </div>  
-            </div>    
-          </div>
-          <div class="wrap-thumbs small-12">
-          	<div class="small-12 large-6 text-center view view-first">  
-                 <img src="r/img/rubro-3.jpg" />  
-                 <div class="mask">  
-                 <h2>Producto</h2>  
-                 <p>Descripción</p>  
-                     <a href="#" class="info">Leer más</a>  
-                 </div>  
-            </div>    
-          </div>
-          <div class="wrap-thumbs small-12">
-          	<div class="small-12 large-6 text-center view view-first">  
-                 <img src="r/img/rubro-1.jpg" />  
-                 <div class="mask">  
-                 <h2>Producto</h2>  
-                 <p>Descripción</p>  
-                     <a href="#" class="info">Leer más</a>  
-                 </div>  
-            </div>    
-          </div>            
+        <div class="small-12 medium-12 large-6 view no-focus">
+            <a href="#">
+                <img src="r/img/rubro-1.jpg" />
+                <div class="mask"></div>
+                <div class="content">
+                    
+                </div>
+            </a>
+        </div>
+        <div class="small-12 medium-12 large-6 view">
+            <img src="r/img/rubro-3.jpg" />
+            <div class="mask"></div>
+            <div class="content">
+                <h2>Focused</h2>
+                <p>Este es el rubro seleccionado y tiene una pequeña descripcion</p>
+            </div>
+        </div>
+        <div class="small-12 medium-12 large-6 view no-focus">
+            <a href="#">
+                <img src="r/img/rubro-1.jpg" />
+                <div class="mask"></div>
+                <div class="content">
+                    <h2>Nombre del rubro</h2>
+                    <p>Esto es la descripcion del rubro, corta y al punto.</p>
+                </div>
+            </a>
+        </div>        
       </article>
       <article class="small-8 medium-8 large-9 column text-center">
-        <div class="small-5 medium-5 large-3 view view-first">  
-             <img src="r/img/articulo-1.jpg" />  
-             <div class="mask">  
-             <h2>Producto</h2>  
-             <p>Descripción</p>  
-                 <a href="#" class="info">Leer más</a>  
-             </div>  
-        </div>  
-        <div class="small-5 medium-5 large-3 view view-first">  
+        
+        <div class="small-6 medium-4 large-2 view view-first">  
              <img src="r/img/articulo-1.jpg" />  
              <div class="mask">  
              <h2>Producto</h2>  
@@ -72,7 +54,7 @@
                  <a href="#" class="info">Leer más</a>  
              </div>  
         </div> 
-        <div class="small-5 medium-5 large-3 view view-first">  
+        <div class="small-6 medium-4 large-2 view view-first">  
              <img src="r/img/articulo-1.jpg" />  
              <div class="mask">  
              <h2>Producto</h2>  
@@ -80,7 +62,55 @@
                  <a href="#" class="info">Leer más</a>  
              </div>  
         </div> 
-        <div class="small-5 medium-5 large-3 view view-first">  
+        <div class="small-6 medium-4 large-2 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div>
+        <div class="small-6 medium-4 large-2 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
+        <div class="small-6 medium-4 large-2 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
+        <div class="small-6 medium-4 large-2 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div>
+        <div class="small-6 medium-4 large-2 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
+        <div class="small-6 medium-4 large-2 view view-first">  
+             <img src="r/img/articulo-1.jpg" />  
+             <div class="mask">  
+             <h2>Producto</h2>  
+             <p>Descripción</p>  
+                 <a href="#" class="info">Leer más</a>  
+             </div>  
+        </div> 
+        <div class="small-6 medium-4 large-2 view view-first">  
              <img src="r/img/articulo-1.jpg" />  
              <div class="mask">  
              <h2>Producto</h2>  
